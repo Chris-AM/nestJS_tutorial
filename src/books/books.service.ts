8933a997a867bc24087ff6093b2e7ea478aaf6ce
chris-am <c.aranguizm@outlook.com>
2022-06-05 17:03:41 -0400

    testing books behaviour

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    findAll(): any {
+        return 'This action returns all books';
+    }
+}
