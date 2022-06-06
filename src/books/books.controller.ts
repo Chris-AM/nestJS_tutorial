b1b48bb195cb0fc251cbe2a79e9b7b57d5aece64
chris-am <c.aranguizm@outlook.com>
2022-06-06 07:28:17 -0400

    adding crud and typing to controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { Book } from './book.class';
+import { BookDto } from './book.dto';
 import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
-    constructor(private booksService: BooksService) {
-
-    }
+    constructor(private booksService: BooksService) {}
 
     @Get()
-    findAll(){
-        return this.booksService.findAll();
+    findAll(@Req() req: Request):Book[] {
+        return this.booksService.findAll(req.query);
     }
-
+    @Post()
+    createBook(@Body() newBook: BookDto): Book{
+        return this.booksService.createBook(newBook);
+    }
+    @Delete(':bookID')
+    deleteBook(@Param('bookID') bookId: string): Book{
+        return this.booksService.deleteBook(bookId)
+    }
+    @Put(':bookId')
+    updateBook(@Param('bookId') bookId: string, @Body() newBook: BookDto): Book{
+        return this.booksService.updateBook(bookId, newBook);
+    }
 }
